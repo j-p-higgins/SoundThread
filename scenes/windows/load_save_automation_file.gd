2d0e15f577ca2288388cffcb43ddb4dcd3c6b10d
Jonathan Higgins <jh03878@gmail.com>
2026-09-16 14:11:35 +0100

    updated some processes to fix their output duration sliders and added a temp ui for testing midi track changes

--- scenes/windows/load_save_automation_file.gd
+++ scenes/windows/load_save_automation_file.gd
@@ -9,8 +9,10 @@ var max_y: float
 var exponential: bool
 
 var automation_points = []
+var midi_import = {}
 
 @onready var error_label = $VBoxContainer/ErrorLabel
+@onready var track_select = $VBoxContainer/MIDITrackSelect
 
 func _ready() -> void:
 	error_label.hide()
@@ -61,11 +63,21 @@ func _on_load_dialog_file_selected(path: String) -> void:
 	#regex for splitting string at whitespace
 	var regex = RegEx.create_from_string("\\S+")
 	
+	track_select.hide()
+	
 	if path.get_extension().to_lower() == "mid":
 		var parser = MIDIParser.new()
 		var midi_info = parser.midi_to_brk(path, true)
 		var midi_data = midi_info["tracks"]
-		if midi_data.size() > 1:
+		if midi_data.size() > 0:
+			track_select.clear()
+			track_select.show()
+			midi_import = midi_data
+			if midi_data.size() > 1:
+				midi_import = midi_data
+				for track in midi_data:
+					track_select.add_item(str(track["track_number"]) + ": " + track["track_name"])
+				track_select.select(0)
 			var track_1 = midi_data[0]
 			var brk = track_1["brk"]
 			automation_loaded.emit(brk)
@@ -205,3 +217,10 @@ func normalised_to_value(t: float) -> float:
 		return exp(lerp(log_min, log_max, t))
 	
 	return lerp(min_y, max_y, t)
+
+
+func _on_midi_track_select_item_selected(index: int) -> void:
+	var track = midi_import[index]
+	var brk = track["brk"]
+	automation_loaded.emit(brk)
+	automation_points = brk
